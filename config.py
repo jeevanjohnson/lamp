067a6a60c3000dd89c3805a1fbaa69f61b60fd5a
coverosu <coverosupersonal@gmail.com>
2020-11-12 23:20:05 -0800

    holy

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ PORT = 8000
 IP = "127.0.0.1"
 PACKET_SIZE = 1024
 HOMEPAGE_HTMLFILE_PATH = './templates/index.html'
+NOT_FOUND_HTMLFILE_PATH = './templates/404.html'
--- handlers.py
+++ handlers.py
@@ -6,6 +6,7 @@ class Handlers:
 	def __init__(self, client, context) -> None:
 		self.client = client
 		self.context = context
+		self.request_type = None
 		self.path = None
 		self.params = {}
 
@@ -14,9 +15,11 @@ class Handlers:
 			return
 		_context = context.decode().replace('\r', '')
 		_context = _context.split('\n')
+		self.context = _context
 		context = _context[0].split(' ')
+		self.request_type = context[0]
 		if '?' in context[1]:
-			self.path = context[1].split('?')[0]
+			self.path = context[1].split('?')[0] + '?'
 			tempname = ''
 			_params = ''.join(context[1].split('?')[1]).split('=')
 			for param in _params:
@@ -37,6 +40,13 @@ class Handlers:
 		headers += "\r\n"
 		headers += f'{html_file}\r\n\r\n'
 		return headers
+
+	def render_json(self, _json):
+		headers = "HTTP/1.1 200 OK\r\n"
+		headers += "Content-Type: text/html; charset=utf-8\r\n"
+		headers += "\r\n"
+		headers += f'{_json}\r\n\r\n'
+		return headers
 	
 	def homepage(self):
 		headers = self.render_template(config.HOMEPAGE_HTMLFILE_PATH)
@@ -45,8 +55,28 @@ class Handlers:
 		self.client.shutdown(socket.SHUT_WR)
 		self.client.close()
 
+	def not_found(self):
+		headers = self.render_template(config.NOT_FOUND_HTMLFILE_PATH)
+		self.client.send((message := headers.encode()))
+		print(f'Sent the following headers: {message.decode()}')
+		self.client.shutdown(socket.SHUT_WR)
+		self.client.close()
+
+	def get_file(self):
+		headers = self.render_json(self.params)
+		self.client.send((message := headers.encode()))
+		print(f'Sent the following headers: {message.decode()}')
+		self.client.shutdown(socket.SHUT_WR)
+		self.client.close()
+
 	def check(self):
 		self.parse(self.context)
 		if self.context:
-			if self.path == '/':
-				self.homepage()
+			try:
+				return {
+					'/': self.homepage,
+					'/get_file?': self.get_file
+				}.get(self.path)()
+			except:
+				return self.not_found()
+
