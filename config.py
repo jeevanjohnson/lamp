077111c66f9401a94d9d2fa150dbc91c8ddc0163
coverosu <coverosupersonal@gmail.com>
2020-11-13 11:38:08 -0800

    sick

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 PORT = 8000
 IP = "127.0.0.1"
 PACKET_SIZE = 1024
+PATH_TO_CSS_FOLDER= "./templates/css"
+PATH_TO_ICON = "./templates/icons/icon.ico"
 HOMEPAGE_HTMLFILE_PATH = './templates/index.html'
 NOT_FOUND_HTMLFILE_PATH = './templates/404.html'
--- handlers.py
+++ handlers.py
@@ -12,16 +12,13 @@ class Handlers:
 		self.params = {}
 		self.pages = {
 		'/': self.homepage, 
-		'/get_file?': self.get_file
+		'/get_file?': self.get_file,
 		}
 
 	def parse(self, context):
 		if not context:
 			return
-		try:
-			_context = context.decode().replace('\r', '')
-		except Exception as e:
-			return print(str(e))
+		_context = context.decode().replace('\r', '')
 		_context = _context.split('\n')
 		self.context = _context
 		context = _context[0].split(' ')
@@ -29,7 +26,7 @@ class Handlers:
 		if '?' in context[1]:
 			self.path = context[1].split('?')[0] + '?'
 			tempname = ''
-			_params = ''.join(context[1].split('?')[1]).split('=')
+			_params = ' '.join(' '.join(''.join(context[1].split('?')[1]).split('=')).split('&')).split(' ')
 			for param in _params:
 				if _params.index(param) + 1 & 1: # odd number check
 					self.params[param] = None
@@ -39,10 +36,30 @@ class Handlers:
 		else:
 			self.path = context[1]
 			return
+	
+	def render_variables(self, html_file):
+		_html_file = html_file.replace('%]','[%').split('[%')
+		for line in _html_file:
+			_index = _html_file.index(line)
+			if '<' in line or '>' in line:
+				continue
+			if 'import' in line:
+				line = line.replace('import','').replace(' ', '').replace('\n','')
+				_html_file[_index] = f'[% import {line} %]'
+				try:
+					with open(f"{config.PATH_TO_CSS_FOLDER}/{line}") as e:
+						style = e.read()
+					_html_file[_index] = _html_file[_index].replace(f"[% import {line} %]".format(line=line), (man := "<style>\n" + style + "</style>"))
+					html_file = ''.join(_html_file)
+				except Exception as l:
+					print(str(l))
+		return html_file
 
 	def render_template(self, path_to_html_file):
 		with open(path_to_html_file, 'r') as f:
 			html_file = f.read()
+		if '[%' and '%]' in html_file:
+			html_file = self.render_variables(html_file)
 		headers = "HTTP/1.1 200 OK\r\n"
 		headers += "Content-Type: text/html; charset=utf-8\r\n"
 		headers += "Connection: keep-alive\r\n"
@@ -64,21 +81,27 @@ class Handlers:
 		self.client.send((message := headers.encode()))
 		this_time = int(time.time() * 1000)
 		mili = this_time - curr_time
-		print(f'Sent the following headers: {message.decode()}\nTook {mili} ms!')
+		print(f'Sent the following headers:\n{message.decode()}\nTook {mili} ms!')
 		self.client.shutdown(socket.SHUT_WR)
 		self.client.close()
 
 	def not_found(self):
+		curr_time = int(time.time() * 1000)
 		headers = self.render_template(config.NOT_FOUND_HTMLFILE_PATH)
 		self.client.send((message := headers.encode()))
-		print(f'Sent the following headers: {message.decode()}')
+		this_time = int(time.time() * 1000)
+		mili = this_time - curr_time
+		print(f'Sent the following headers:\n{message.decode()}\nTook {mili} ms!')
 		self.client.shutdown(socket.SHUT_WR)
 		self.client.close()
 
 	def get_file(self):
+		curr_time = int(time.time() * 1000)
 		headers = self.render_json(self.params)
 		self.client.send((message := headers.encode()))
-		print(f'Sent the following headers: {message.decode()}')
+		this_time = int(time.time() * 1000)
+		mili = this_time - curr_time
+		print(f'Sent the following headers:\n{message.decode()}\nTook {mili} ms!')
 		self.client.shutdown(socket.SHUT_WR)
 		self.client.close()
 
