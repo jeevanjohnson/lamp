99f4de1e69f565aebbab75f4a69593a202c09174
coverosu <coverosupersonal@gmail.com>
2021-01-07 13:31:26 -0800

    version update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name = "WebLamp",
-	version = "0.0.5",
+	version = "0.0.6",
 	author = "coverosu",
 	author_email = email,
 	description = "Webserver (with some utilities) made in sockets!",
