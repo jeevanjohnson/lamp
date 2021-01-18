85c559e822c3ea497423c4addfc65c9ae51d805b
coverosu <coverosupersonal@gmail.com>
2021-01-18 15:50:23 -0800

    whoops

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name = "WebLamp",
-	version = "0.1.5",
+	version = "0.1.6",
 	author = "coverosu",
 	author_email = email,
 	description = "Webserver (with some utilities) made in sockets!",
