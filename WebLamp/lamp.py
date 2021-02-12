5bb12de335e10b45904d65b0f0ebd7acda100656
coverosu <coverosupersonal@gmail.com>
2021-02-12 12:44:50 -0800

    man5?

--- WebLamp/lamp.py
+++ WebLamp/lamp.py
@@ -70,7 +70,7 @@ class Lamp:
              method: tuple = ()) -> Callable:
         def inner(func: Callable) -> Callable:
             nonlocal method
-            if not isinstance(path, (str, re.Pattern)) or isinstance(domain, (str, re.Pattern)):
+            if not isinstance(path, (str, re.Pattern)) or not isinstance(domain, (str, re.Pattern)):
                 raise Exception(
                 'Both `path` and `domain` need to be either a `str` '
                 'or a `re.Pattern`'
